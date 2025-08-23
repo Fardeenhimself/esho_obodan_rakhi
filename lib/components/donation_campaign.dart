b66d2d0e337e95dc81184f34a39fae6774fdc839
fardeenhimself <fardeenviper77@gmail.com>
2025-08-23 17:07:17 +0600

    donation campaign selected

--- lib/components/donation_campaign.dart
+++ lib/components/donation_campaign.dart
@@ -12,31 +12,36 @@ class DonationCampaign extends StatelessWidget {
     final formatDate = DateFormat.yMMMMd().format(category.createdAt);
     return Padding(
       padding: const EdgeInsets.all(12.0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Theme.of(
-            context,
-          ).colorScheme.inversePrimary.withAlpha((0.5 * 255).round()),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                category.name,
-                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.inverseSurface,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.5,
+      child: GestureDetector(
+        onTap: () {
+          // navigate to that category detail
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Theme.of(
+              context,
+            ).colorScheme.inversePrimary.withAlpha((0.5 * 255).round()),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  category.name,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.inverseSurface,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5,
+                  ),
                 ),
-              ),
-              Divider(color: Theme.of(context).colorScheme.inverseSurface),
-              Text('তারিখঃ  $formatDate'),
-              const SizedBox(height: 9),
-              Text('মোট অর্থ সংগ্রহঃ'),
-            ],
+                Divider(color: Theme.of(context).colorScheme.inverseSurface),
+                Text('তারিখঃ  $formatDate'),
+                const SizedBox(height: 9),
+                Text('মোট অর্থ সংগ্রহঃ'),
+              ],
+            ),
           ),
         ),
       ),
