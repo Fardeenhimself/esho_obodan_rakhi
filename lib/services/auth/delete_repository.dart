c93f4e6047d81a8f9cf15c4153a19557ea3edbb4
Fardeen Been Zaman <110120932+Fardeenhimself@users.noreply.github.com>
2025-08-29 23:55:26 +0600

    Update delete_repository.dart

--- lib/services/auth/delete_repository.dart
+++ lib/services/auth/delete_repository.dart
@@ -4,7 +4,7 @@ import 'package:islamic_app/services/core/secure_storage_service.dart';
 class DeleteRepository {
   Future<bool> deleteUser(String id) async {
     final token = await SecureStorageService.read('auth_token');
-    final url = Uri.parse('https://halaqa.theabacuses.com/api/user/$id');
+    final url = Uri.parse('YOUR API KEY/$id');
 
     final response = await http.delete(
       url,
